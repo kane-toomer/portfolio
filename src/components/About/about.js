3fa9c4052162b3a3c64e20b5cd2c14c4721a190f
Kane Toomer <kanetoomer@yahoo.com>
2022-07-04 18:06:15 -0700

    about copy done. just need picture of me.

--- src/components/About/about.js
+++ src/components/About/about.js
@@ -23,13 +23,22 @@ class About extends Component {
                         {/* Synopsis */}
                         <div className='col-xs-12 col-lg-6'>
                             <div className=''>
-                                <h3 className='text-white'>My name is Kane Toomer. I'm a software developer from the incredibly hot Las Vegas, NV.</h3>
+                                <h3 className='text-white mb-4'>My name is Kane Toomer. I'm a software developer from the incredibly hot Las Vegas, NV.</h3>
 
                                 {/* Paragraph 1 */}
-                                <p></p>
+                                <p className="mb-4">My path to tech is not traditional by any means. I thought my life would be simple, get my degree, join the military, do my 20, retire and die (aka follow the family mold). I realised a little too late that this wasn't going to be for me.</p>
 
                                 {/* Paragraph 2 */}
-                                <p></p>
+                                <p className='mb-4'> Instead I work in higher education and I am ready for a change. As much as I love working at the collegiate level, working in tech as a software developer is where I truly want to be.</p>
+
+                                {/* Paragraph 3 */}
+                                <p className='mb-4'>I started coding in high school by making small iOS apps. This carried on into my undergrad where I would spend most of my time designing small apps in Sketch during lectures and coding them in the library right after. This grew into building a (failed) startup w/ a friend where I designed and developed the front-end for both the mobile and web applications of our property management software.</p>
+
+                                {/* Paragraph 4 */}
+                                <p className='mb-4'>I think it's safe to say I think I should have seen this coming. However, when you are young, sometimes...
+                                </p>
+                                <p className='text-center'>"you have to go through it, to get to it"</p>
+                                <p className='text-center mb-5'>- I have no clue who's quote this is... lol</p>
 
                                 <div className='project-card'>
                                     <div className='row'>
@@ -39,10 +48,8 @@ class About extends Component {
                                             <p><span className='card-titles'>Frontend:</span>
                                                 <ul>
                                                     <li>React.js</li>
+                                                    <li>React Native</li>
                                                     <li>Redux</li>
-                                                    <li>Javascript</li>
-                                                    <li>HTML</li>
-                                                    <li>CSS</li>
                                                     <li>Bootstrap</li>
                                                 </ul>
                                             </p>
@@ -64,7 +71,7 @@ class About extends Component {
                                                 <ul>
                                                     <li>Git/Github</li>
                                                     <li>Heroku</li>
-                                                    <li>Figma (used for wireframing and UI/X design for web and mobile applications)</li>
+                                                    <li>Figma</li>
                                                 </ul>
                                             </p>
                                         </div>
